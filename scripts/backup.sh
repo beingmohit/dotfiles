93390d17f2cfb50c5d51814ce8c50bc48df635ba
Mohit Agrawal <beingmohit09@gmail.com>
2019-01-01 12:32:45 +0530

    Changes

--- scripts/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash   
-echo "Copying documents .... "
-rclone copy /home/mohit/documents drive:documents -v
-
-echo "Copying creds (only gpg) .... "
-rclone copy /home/mohit/creds/ drive:creds -v --include="*.gpg"
