931bbf780b0df03057d65713b53ff60ef05132d3
Mohit Agrawal <mohit@oviyum.com>
2019-03-06 15:04:19 +0530

    WIP

--- scripts/clean/clean.sh
+++ scripts/clean/clean.sh
@@ -5,5 +5,21 @@ docker rm -v $(docker ps -a -q -f status=exited) || true
 docker rmi $(docker images -f "dangling=true" -q) || true
 docker volume prune || true
 
+#blacklist
+#pacnew 
+#pythone packages /usr/lib/py*
+#.cache files
+#~/home whitelist
+#unsed users and groups
+#/root
+#broken symlinks
+#npm bin in /usr/bin
 
+#orphans
+sudo pacman -Rns $(pacman -Qtdq) || true 
 
+#pacman -S $(pacman -Qq | grep python-) 
+
+#reqired by pythone: pacman -Qi python
+
+# list packages pacman -Qe
--- scripts/clean/lostfiles.sh
+++ scripts/clean/lostfiles.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
 set -e
 
-LOSTFILES=$(lostfiles)
+# LOSTFILES=$(lostfiles)
 
-echo $LOSTFILES | while read line ; do
-   echo === $line ===
+# echo $LOSTFILES | while read line ; do
+#    echo
+#    echo === $line ===
+#    echo
+# done
+
+LOSTFILES=()
+WHITELIST=()
+BLACKLIST=()
+
+while read -r line; do
+   LOSTFILES+=($line)
+done < <(lostfiles)
+
+while read -r line; do
+   WHITELIST+=($line)
+done < <(cat lists/whitelist)
+
+while read -r line; do
+   BLACKLIST+=($line)
+done < <(cat lists/blacklist)
+
+for i in "${LOSTFILES[@]}"; do
+   for j in "${WHITELIST[@]}"; do
+      if [ $i == $j ]; then
+         continue 2
+      fi
+
+      if [[ $i =~ $j ]]; then
+         continue 2
+      fi
+   done
+
+   for k in "${BLACKLIST[@]}"; do
+      if [ $i == $k ]; then
+         continue 2
+      fi
+
+      if [[ $i =~ $k ]]; then
+         continue 2
+      fi
+   done
+
+   echo $i
 done
