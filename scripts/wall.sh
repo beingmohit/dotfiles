18230478cb9abeeba19565feebc9bb94eb8f771f
Mohit Agrawal <beingmohit09@gmail.com>
2020-04-19 01:08:36 +0530

    minor changers

--- scripts/wall.sh
+++ scripts/wall.sh
@@ -1,31 +1,16 @@
 #/bin/bash
-# Settings
-RUN_AS_DAEMON=0                           # Run unspla.sh as a daemon if 1, if 0 unspla.sh will set the image then exit
-BACKGROUND_RES="1920x1080"                # Set the size of unsplash images to pull
-PARAMETERS="backgrounds"                  # Set any other parametes to pull in images. add a comma between parameters ( nature, backgrounds)
-BACKGROUND_COMMAND="feh --bg-fill "       # The command that will be used to set the background, default is feh.  Image will be supplied after the command.
-IMAGE_SAVE_DIR="/tmp/unsplash/photo.jpg"  # Directory to save the images to
 
-#Do not change unless you need to
-UNSPLASH_URL="https://source.unsplash.com/random/$BACKGROUND_RES/?$PARAMETERS"
+UNSPLASH_URL="https://source.unsplash.com/random/1920x1080/?wallpapers"
 
-# Daemon Settings (only modify if you are using unspla.sh as a daemon)
-UPDATE_INTERVAL=28800                     # How long to wait before changing the picture (seconds)
+mkdir -p ~/.unsplash
 
-mkdir -p /tmp/unsplash
+echo "fetching file"
+wget $UNSPLASH_URL -4 -O /tmp/unsplash.jpg && mv /tmp/unsplash.jpg ~/.unsplash/photo.jpg
 
-if [ $RUN_AS_DAEMON = 1 ]
-then
-  printf "Launching into background"
-  while :
-  do
-    wget --quiet $UNSPLASH_URL -4 -O $IMAGE_SAVE_DIR
-    $BACKGROUND_COMMAND $IMAGE_SAVE_DIR
-    ~/scripts/lock.sh -u $IMAGE_SAVE_DIR 
-    sleep $UPDATE_INTERVAL
-  done &
-else
-    wget --quiet $UNSPLASH_URL -4 -O $IMAGE_SAVE_DIR
-    ~/scripts/lock.sh -u $IMAGE_SAVE_DIR 
-    $BACKGROUND_COMMAND $IMAGE_SAVE_DIR
+if [ -f ~/.unsplash/photo.jpg ]; then
+  echo "setting backgroud"
+  feh --bg-fill ~/.unsplash/photo.jpg
+
+  echo "setting lock"
+  ~/scripts/lock.sh -u ~/.unsplash/photo.jpg
 fi
